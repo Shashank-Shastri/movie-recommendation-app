3980e8b4967f97466d994ffa770a80949d993ac1
Shashank-Shastri <shashank.m.shastri@gmail.com>
2021-04-18 09:20:38 -0700

    Add carousel and fix styling

---
+++ src/components/SlideShow.vue
@@ -0,0 +1,58 @@
+<template>
+    <Carousel :itemsToShow="3" v-if="items.length">
+        <Slide v-for="{ id, imageUrl, link = '', title = '' } in items" :key="id" >
+            <a :href="link" target="_blank"><img style="max-width: 18rem;" class="card-img" :src="imageUrl" :alt="title"></a>
+        </Slide>
+
+        <template #addons>
+            <Navigation />
+            <Pagination />
+        </template>
+    </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
+
+import 'vue3-carousel/dist/carousel.css';
+
+export default defineComponent({
+    name: 'SlideShow',
+    props: {
+        items: {
+            type: Array
+        }
+    },
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation
+    }
+});
+</script>
+
+<style>
+.carousel__item {
+    min-height: 200px;
+    width: 100%;
+    background-color: var(--carousel-color-primary);
+    color: var(--carousel-color-white);
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+    box-sizing: content-box;
+    border: 5px solid white;
+}
+</style>
--- src/components/StarterPage.vue
+++ src/components/StarterPage.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="starter-page">
         <div class="section text-center">
-            <h3>Search a movie you like</h3>
-            <v-select @search="onSearch" :filterable="false" :options="movies" v-model="selectedMovie" label="l">
+            <SlideShow :items="slides"/>
+            <v-select
+                @search="onSearch"
+                :filterable="false"
+                :options="movies"
+                v-model="selectedMovie"
+                label="l"
+                placeholder="Type a movie you like"
+            >
                 <template v-slot:no-options>
                     type to search IMDb movies..
                 </template>
@@ -48,23 +55,89 @@
 </template>
 <script>
 import axios from 'axios';
-import { debounce, get } from 'lodash';
+import { debounce, get, shuffle } from 'lodash';
 import MovieInfo from './MovieInfo.vue';
 import fetchJsonp from 'fetch-jsonp';
+import SlideShow from './SlideShow.vue';
 
-const _ = { debounce, get };
+const _ = { debounce, get, shuffle };
 
 export default {
     name: 'starter',
     components: {
-        MovieInfo
+        MovieInfo,
+        SlideShow
     },
     data() {
         return {
-            selectedMovie: '',
             movies: [],
-            recommendedMovies: []
+            recommendedMovies: [],
+            selectedMovie: '',
+            slides: [{
+                'id': 'tt0111161',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0111161/',
+                'title': 'The Shawshank Redemption'
+            }, {
+                'id': 'tt0068646',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0068646/',
+                'title': 'The Godfather'
+            }, {
+                'id': 'tt0071562',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0071562/',
+                'title': 'The Godfather: Part II'
+            }, {
+                'id': 'tt0468569',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0468569/',
+                'title': 'The Dark Knight'
+            }, {
+                'id': 'tt0050083',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0050083/',
+                'title': '12 Angry Men'
+            }, {
+                'id': 'tt0108052',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0108052/',
+                'title': 'Schindler\'s List'
+            }, {
+                'id': 'tt0167260',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0167260/',
+                'title': 'The Lord of the Rings: The Return of the King'
+            }, {
+                'id': 'tt0110912',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0110912/',
+                'title': 'Pulp Fiction'
+            }, {
+                'id': 'tt0060196',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BOTQ5NDI3MTI4MF5BMl5BanBnXkFtZTgwNDQ4ODE5MDE@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0060196/',
+                'title': 'The Good, the Bad and the Ugly'
+            }, {
+                'id': 'tt0120737',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0120737/',
+                'title': 'The Lord of the Rings: The Fellowship of the Ring'
+            }, {
+                'id': 'tt0137523',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0137523/',
+                'title': 'Fight Club'
+            }, {
+                'id': 'tt0109830',
+                'imageUrl': 'https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX674_.jpg',
+                'link': 'https://www.imdb.com/title/tt0109830/',
+                'title': 'Forrest Gump'
+            }]
         };
+    },
+    beforeMount() {
+        this.slides = _.shuffle(this.slides);
     },
     watch: {
         selectedMovie() {
@@ -147,23 +220,10 @@ img {
     max-height: 50px;
     margin-right: 0.5rem;
 }
+@import '../assets/v-select-theme.scss';
 
-.v-select .dropdown li {
-    border-bottom: 1px solid rgba(112, 128, 144, 0.1);
-}
-
-.v-select .dropdown li:last-child {
-    border-bottom: none;
-}
-
-.v-select .dropdown li a {
-    padding: 10px 20px;
-    width: 100%;
-    font-size: 1.25em;
-    color: #3c3c3c;
-}
-
-.v-select .dropdown-menu .active > a {
-    color: #fff;
+.vs__dropdown-toggle {
+    border-radius: 5px !important;
+    border-color: whitesmoke !important;
 }
 </style>
