877ac567471302dce8ddb210625d674abdcb96e2
Shashank-Shastri <shashank.m.shastri@gmail.com>
2023-12-19 12:54:21 +0530

    Fix more lint errors

--- src/components/HeaderLinks.vue
+++ src/components/HeaderLinks.vue
@@ -4,8 +4,8 @@
             ><img src="/assets/favicon.png"
         /></router-link>
         <div id="nav">
-            <router-link :to="{ name: 'Home' }">Home</router-link> |
-            <router-link :to="{ name: 'About' }">About</router-link>
+            <router-link :to="{ name: 'HomePage' }">Home</router-link> |
+            <router-link :to="{ name: 'AboutPage' }">About</router-link>
         </div>
     </div>
 </template>
